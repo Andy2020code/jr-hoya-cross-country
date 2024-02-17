f4839f3d77a64fead385d0eeb420e42ab8637cb1
Andy2020code <144275177+Andy2020code@users.noreply.github.com>
2024-02-17 17:33:10 +0000

    design update

--- server/main/views.py
+++ server/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/home-page.html')
+
+def about_page(request):
+    return render (request, 'main/school-about-page.html')
